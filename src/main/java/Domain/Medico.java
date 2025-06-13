0514aeedb032f635104c3582460f69bc1edaabf5
claudio gomez <clauivangomez48@gmail.com>
2025-06-13 13:45:33 -0300

    Reestructura del proyecto: - Agregado .gitignore - Eliminadas clases antiguas de com.consultorio.proyecto - Añadidas clases Medico, Paciente, Turno en 'Domain' - Nueva clase Main en 'Principal' - Interfaz Vista en paquete 'Vista' - Actualizado pom.xml

---
+++ src/main/java/Domain/Medico.java
@@ -0,0 +1,14 @@
+package Domain;
+import java.util.HashMap;
+
+public class Medico {
+  private int id;
+  private String nombre;
+  private String especialidad;
+  
+  public Medico(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+  
+}
--- src/main/java/com/consultorio/proyecto/Main.java
+++
@@ -1,8 +0,0 @@
-package com.consultorio.proyecto;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
